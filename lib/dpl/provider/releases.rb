32d14b22eeebb1c53d75bd9aeea38a7c19b51537
Adam King <rak@us.ibm.com>
2017-02-15 17:35:55 -0500

    more debug2!

--- lib/dpl/provider/releases.rb
+++ lib/dpl/provider/releases.rb
@@ -104,6 +104,7 @@ module DPL
         if tag_matched == false
           log "Creating a release #{get_tag}"
           release_url = api.create_release(slug, get_tag, options.merge({:draft => true})).rels[:self].href
+          log "Created release #{get_tag}"
           log "release_url==#{release_url}"
         end
 
